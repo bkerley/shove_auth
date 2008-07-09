513e7ad4f4c50ac30f4a2adbde6936683612d779
Bryce Kerley <bryce@worldmedia.net>
2008-07-09 15:19:49 -0400

    generate randomness

--- app/models/nonce.rb
+++ app/models/nonce.rb
@@ -1,2 +1,8 @@
 class Nonce < ActiveRecord::Base
+  before_create :creation_generator
+  
+  def creation_generator
+    self.nonce = Base64.encode64(OpenSSL::Random.random_bytes(30)).chomp
+    self.sid = Base64.encode64(OpenSSL::Random.random_bytes(30)).chomp
+  end
 end
