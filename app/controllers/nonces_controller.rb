52a1a0de577d52fd3400d7c96985b4c637419357
Bryce Kerley <bryce@worldmedia.net>
2008-07-09 16:27:13 -0400

    remove nonsense nonces scaffold parts

--- app/controllers/nonces_controller.rb
+++ app/controllers/nonces_controller.rb
@@ -11,67 +11,6 @@ class NoncesController < ApplicationController
     end
   end
 
-  # GET /nonces/1
-  # GET /nonces/1.xml
-  def show
-    @nonce = Nonce.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @nonce }
-    end
-  end
-
-  # GET /nonces/new
-  # GET /nonces/new.xml
-  def new
-    @nonce = Nonce.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @nonce }
-    end
-  end
-
-  # GET /nonces/1/edit
-  def edit
-    @nonce = Nonce.find(params[:id])
-  end
-
-  # POST /nonces
-  # POST /nonces.xml
-  def create
-    @nonce = Nonce.new(params[:nonce])
-
-    respond_to do |format|
-      if @nonce.save
-        flash[:notice] = 'Nonce was successfully created.'
-        format.html { redirect_to(@nonce) }
-        format.xml  { render :xml => @nonce, :status => :created, :location => @nonce }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @nonce.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /nonces/1
-  # PUT /nonces/1.xml
-  def update
-    @nonce = Nonce.find(params[:id])
-
-    respond_to do |format|
-      if @nonce.update_attributes(params[:nonce])
-        flash[:notice] = 'Nonce was successfully updated.'
-        format.html { redirect_to(@nonce) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @nonce.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /nonces/1
   # DELETE /nonces/1.xml
   def destroy
