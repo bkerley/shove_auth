31ac61c7d249bcc95f342aed62f0c6abef499492
Bryce Kerley <bryce@worldmedia.net>
2008-07-09 14:52:19 -0400

    password -> digest

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,16 @@
 class Account < ActiveRecord::Base
+  
+  def password=(password)
+    self.digest = Account.digest(self.username, password)
+  end
+  
+  def check_digest(password)
+    return self.digest == Account.digest(self.username, password)
+  end
+  
+  def self.digest(username, password)
+    sha1 = OpenSSL::Digest::Digest.new('SHA1')
+    sha1 << username << new_pass
+    return Base64.encode64(sha1.digest).chomp
+  end
 end
