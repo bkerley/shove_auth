4bed23046657c8f416fa8bee87fdcc0328f0ea9a
Bryce Kerley <bryce@worldmedia.net>
2008-07-09 17:07:14 -0400

    make sure the nonce is valid

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -12,6 +12,16 @@ class SessionController < ApplicationController
 
   def show
     n = Nonce.find_by_sid(params[:id])
+    if n.nil?
+      render(:text=>'404 Not Found', :status=>404) 
+      return
+    end
+    
+    if n.state == :not_authenticated
+      render(:text=>'403 Forbidden', :status=>403)
+      return
+    end
+    
     respond_to do |wants|
       wants.json { render :json=>n.to_json }
       wants.xml { render :xml=>n.to_xml }
