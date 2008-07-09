9601b53000153623d7f56c633d9d52a465f4e10b
Bryce Kerley <bryce@worldmedia.net>
2008-07-09 16:21:38 -0400

    Fill in the create method

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,5 +1,10 @@
 class SessionController < ApplicationController
   def create
+    n = Nonce.create
+    respond_to do |wants|
+      wants.json { render :json=>n.to_json }
+      wants.xml { render :xml=>n.to_xml }
+    end
   end
 
   def update
