053103296b88f99218e804feac5718f8a0cae66f
Bryce Kerley <bryce@worldmedia.net>
2008-07-09 15:01:08 -0400

    flunking the check_digest test

--- test/unit/account_test.rb
+++ test/unit/account_test.rb
@@ -11,4 +11,11 @@ class AccountTest < ActiveSupport::TestCase
     a = Account.find_by_username('bkerley')
     assert_equal a.id, @account.id
   end
+  
+  def test_hashing
+    myhash = "jK+2mdV5UlqMcoet5RmsqejZ7Gs="
+    assert_equal Account.digest('bkerley', 'butt'), myhash
+    assert_equal @account.digest, myhash
+    assert @account.check_digest(myhash)
+  end
 end
