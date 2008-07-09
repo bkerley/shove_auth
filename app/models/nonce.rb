c4a13a9121fcccbf0a11f1451eeea85d36c80322
Bryce Kerley <bryce@worldmedia.net>
2008-07-09 15:53:30 -0400

    move load_user into the instance

--- app/models/nonce.rb
+++ app/models/nonce.rb
@@ -7,12 +7,22 @@ class Nonce < ActiveRecord::Base
     self.sid = Base64.encode64(OpenSSL::Random.random_bytes(30)).chomp
   end
   
+  def state
+    return "authenticated" if self.user_id
+  end
+  
+  def load_user(user, hmac)
+    check = Nonce.hmac(user.digest, "PUT /session/#{sid} #{nonce}")
+    return false unless check == hmac
+    self.user_id = user.id
+    return user
+  end
+  
   def self.load_user(sid, username, hmac)
     user = Account.find_by_username(username)
     nonce = find_by_sid(sid)
-    check = hmac(user.digest, "PUT /session/#{sid} #{nonce.nonce}")
-    return false unless check == hmac
-    nonce.user_id = user.id
+    r = nonce.load_user(user, hmac)
+    return false unless r
     nonce.save
     return user
   end
