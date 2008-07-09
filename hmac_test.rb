a7fd498d9112e8626660e6db024c4b390253e0d2
Bryce Kerley <bryce@worldmedia.net>
2008-07-09 14:30:07 -0400

    source the test vectors

--- hmac_test.rb
+++ hmac_test.rb
@@ -1,5 +1,7 @@
 require 'openssl'
 vectors = [
+  # examples from appendix A of FIPS-198a
+  # http://csrc.nist.gov/publications/fips/fips198/fips-198a.pdf
   {:k=>(0x00..0x3f), :t => 'Sample #1', :c=>'4f4ca3d5d68ba7cc0a1208c9c61e9c5da0403c0a'},
   {:k=>(0x30..0x43), :t => 'Sample #2', :c=>'0922d3405faa3d194f82a45830737d5cc6c75d24'},
   {:k=>(0x50..0xb3), :t => 'Sample #3', :c=>'bcf41eab8bb2d802f3d05caf7cb092ecf8d1a3aa'}
