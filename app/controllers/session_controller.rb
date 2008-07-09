4026ec3b6a385e5090037e7a1e4ade69f1e23fc7
Bryce Kerley <bryce@worldmedia.net>
2008-07-09 16:32:54 -0400

    fill out the session/show action

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -11,6 +11,11 @@ class SessionController < ApplicationController
   end
 
   def show
+    n = Nonce.find_by_sid(params[:id])
+    respond_to do |wants|
+      wants.json { render :json=>n.to_json }
+      wants.xml { render :xml=>n.to_xml }
+    end
   end
 
 end
