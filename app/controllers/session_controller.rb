764d1da24ad648c43f524ecb0f0c381792542a0c
Bryce Kerley <bryce@worldmedia.net>
2008-07-09 18:09:28 -0400

    prepared some spacing, also put in a location field that I'm going to take out tomorrow morning

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,6 +1,7 @@
 class SessionController < ApplicationController
   def create
     n = Nonce.create
+    headers[:location] = "/session/#{n.sid}"
     respond_to do |wants|
       wants.json { render :json=>n.to_json }
       wants.xml { render :xml=>n.to_xml }
@@ -8,10 +9,23 @@ class SessionController < ApplicationController
   end
 
   def update
+    
+    user = Nonce.load_user(params[:id], params[:username], params[:hmac])
+    
+    unless user
+      render(:text=>'400 Bad Request', :status=>403)
+      return
+    end
+    
+    
   end
 
   def show
-    n = Nonce.find_by_sid(params[:id])
+    sid = params[:id]
+    
+    n = Nonce.find_by_sid(sid)
+    
+    expected_hmac = hmac(n.session_secret, '')
     if n.nil?
       render(:text=>'404 Not Found', :status=>404) 
       return
@@ -21,6 +35,7 @@ class SessionController < ApplicationController
       render(:text=>'403 Forbidden', :status=>403)
       return
     end
+    
     
     respond_to do |wants|
       wants.json { render :json=>n.to_json }
