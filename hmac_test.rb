784c1edd4c1d606dcff582b4360d46a15acd7fa7
Bryce Kerley <bryce@worldmedia.net>
2008-07-09 10:16:12 -0400

    hmac verification

--- hmac_test.rb
+++ hmac_test.rb
@@ -1,5 +1,13 @@
 require 'openssl'
-d = OpenSSL::Digest::Digest.new('SHA1')
-k = (0x30..0x43).to_a
-t = "Sample #2"
-puts OpenSSL::HMAC.hexdigest(d, k.map{|c|c.chr}.join(''), t) == "0922d3405faa3d194f82a45830737d5cc6c75d24"
+vectors = [
+  {:k=>(0x00..0x3f), :t => 'Sample #1', :c=>'4f4ca3d5d68ba7cc0a1208c9c61e9c5da0403c0a'},
+  {:k=>(0x30..0x43), :t => 'Sample #2', :c=>'0922d3405faa3d194f82a45830737d5cc6c75d24'},
+  {:k=>(0x50..0xb3), :t => 'Sample #3', :c=>'bcf41eab8bb2d802f3d05caf7cb092ecf8d1a3aa'}
+]
+vectors.each do |v|
+  d = OpenSSL::Digest::Digest.new('SHA1')
+  k = v[:k]
+  t = v[:t]
+  c = v[:c]
+  puts OpenSSL::HMAC.hexdigest(d, k.map{|e|e.chr}.join(''), t) == c
+end
