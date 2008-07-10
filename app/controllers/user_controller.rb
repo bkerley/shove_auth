6076c5cb5f2d233e61de4516ddbc8ca658b59453
Bryce Kerley <bryce@worldmedia.net>
2008-07-10 11:57:15 -0400

    session_secret verification

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,10 @@ class UserController < ApplicationController
   end
 
   def show
-    user = Account.find_by_username params[:id]
+    username = params[:id]
+    verify("GET /user/#{username} %s") or return
+    
+    user = Account.find_by_username username
     
     respond_to do |wants|
       wants.xml {  render :xml=>user.to_xml }
@@ -16,5 +19,22 @@ class UserController < ApplicationController
 
   def destroy
   end
-
+  
+  private
+  def verify(content)
+    sid = params[:session][:sid]
+    verifier = content.printf(sid)
+    hmac = params[:session][:hmac]
+    session_secret = Nonce.find_by_sid(sid).session_secret
+    
+    check = hmac(session_secret, verifier)
+    
+    return true if check == hmac
+    
+    render :text=>'403 Forbidden', :status=>403
+    return false
+  rescue
+    render :text=>'403 Forbidden', :status=>403
+    return false
+  end
 end
