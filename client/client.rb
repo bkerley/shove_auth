948326195c537a3817c05dcebe613c179d2563d3
Bryce Kerley <bryce@worldmedia.net>
2008-07-10 10:36:47 -0400

    get closer to working

--- client/client.rb
+++ client/client.rb
@@ -1,11 +1,51 @@
 require 'active_resource'
+require 'openssl'
 
 SITE = 'http://localhost:3001/'
-class Session < ActiveResource::Base
-  self.site = SITE
-  self.collection_name = 'session'
+
+module ShoveAuthClient
+  class Session < ActiveResource::Base
+    self.site = SITE
+    self.collection_name = 'session'
+
+    def create
+      returning connection.post(collection_path, to_xml, self.class.headers) do |response|
+        load_attributes_from_response(response)
+        self.id = CGI.escape(self.sid)
+      end
+    end
+    
+    def authenticate(username, password)
+      self.username = username
+      self.hmac = hmac(digest(username, password), "PUT /session/#{self.sid} #{self.nonce}")
+      self.save
+      return self.state
+    end
+    
+    private
+    def hmac(secret, message)
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('SHA1'), secret, message)
+    end
+    
+    def digest(username, password)
+      sha1 = OpenSSL::Digest::Digest.new('SHA1')
+      sha1 << username << password
+      return Base64.encode64(sha1.digest).chomp
+    end
+  end
+  class User < ActiveResource::Base
+    self.site = SITE
+    self.collection_name = 'user'
+  end
+  
+  class Client
+    def initialize
+      @session = Session.create
+    end
+    
+    def login(username, password)
+      @session.authenticate(username, password)
+    end
+  end
 end
-class User < ActiveResource::Base
-  self.site = SITE
-  self.collection_name = 'user'
-end
+
