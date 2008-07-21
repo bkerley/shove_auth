ece39ba13c295afbe9a15217f5cb716726f4a5e6
Bryce Kerley <bryce@worldmedia.net>
2008-07-21 15:29:54 -0400

    Login requirements

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -15,4 +15,17 @@ class ApplicationController < ActionController::Base
   def method_missing(methodname, *args)
    render :text=>'404 Not Found', :status=>404
   end
+  
+  def require_login
+    unless session[:account]
+      redirect_to new_admin_path
+    end
+  end
+  
+  def require_admin
+    unless session[:account] && Account.find(session[:account]).admin
+      flash[:notice] = "You're not allowed to do that."
+      redirect_to admin_path(session[:account])
+    end
+  end
 end
