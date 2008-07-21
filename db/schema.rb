4c29536a7e76da0bff4fb720290f3f7bb958d451
Bryce Kerley <bryce@worldmedia.net>
2008-07-21 13:52:14 -0400

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080709212531) do
+ActiveRecord::Schema.define(:version => 20080721175056) do
 
   create_table "accounts", :force => true do |t|
     t.string   "username"
     t.string   "digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   create_table "nonces", :force => true do |t|
