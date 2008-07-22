4bcf8d9606e11aebf54aed30d4b9733ed16cc0e6
Bryce Kerley <bryce@worldmedia.net>
2008-07-22 15:40:16 -0400

    capistrano deployment tools

---
+++ config/deploy.rb
@@ -0,0 +1,19 @@
+set :application, "shove_auth"
+set :repository,  "git@github.com:bkerley/shove_auth.git"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/home/shove/apps/#{application}"
+set :user, 'shove'
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1
+
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+
+role :app, "tech.worldmedia.net"
+role :web, "tech.worldmedia.net"
+role :db,  "tech.worldmedia.net", :primary => true
