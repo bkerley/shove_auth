edfcf6e20a63ea9cdacc74941bf58cfa0c9dde79
Bryce Kerley <bryce@worldmedia.net>
2008-07-25 17:18:02 -0400

    failing uncredentialed tests

--- test/functional/user_controller_test.rb
+++ test/functional/user_controller_test.rb
@@ -1,8 +1,31 @@
 require 'test_helper'
 
 class UserControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  context 'without credentials' do
+    context 'on GET' do
+      setup do
+        get :show, :id=>'invalid'
+      end
+      should_respond_with 403
+    end
+    context 'on PUT' do
+      setup do
+        put :update, :id=>'invalid'
+      end
+      should_respond_with 403
+    end
+    context 'on DELETE' do
+      setup do
+        put :destroy, :id=>'invalid'
+      end
+      should_respond_with 403
+    end
+    context 'on POST' do
+      setup do
+        post :create, :id=>'invalid'
+      end
+      should_respond_with 403
+    end
   end
+
 end
