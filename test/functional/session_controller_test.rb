d2de8f9468c251358c6c1e00da2735ce7b74144e
Bryce Kerley <bryce@worldmedia.net>
2008-07-25 16:15:55 -0400

    test the creation of nonces

--- test/functional/session_controller_test.rb
+++ test/functional/session_controller_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class SessionControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  context "on POST to create" do
+    setup do
+      request_xml
+      post :create
+    end
+    
+    should_not_set_the_flash
+    should_respond_with :success
+    should_respond_with_xml_for 'nonce'
   end
 end
