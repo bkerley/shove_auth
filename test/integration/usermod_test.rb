1d1092e7a002531fbccf4a447f312ed3ce1594a0
Bryce Kerley <bryce@worldmedia.net>
2008-07-28 16:52:31 -0400

    More exhaustive, manage state

--- test/integration/usermod_test.rb
+++ test/integration/usermod_test.rb
@@ -16,18 +16,29 @@ class UsermodTest < ActionController::IntegrationTest
       setup do
         @c.login 'bkerley', 'butt'
       end
+      should 'show myself' do
+        assert @c.user
+        assert_equal 'bkerley', @c.user.username
+      end
       should 'change own password' do
-        @c.password = 'bloat'
+        @c.user.set_password 'bloat'
         assert_equal :authenticated, ShoveAuth::login('bkerley', 'bloat')
+        @c.user.set_password 'butt'
+        assert_equal :authenticated, ShoveAuth::login('bkerley', 'butt')
       end
       should "change failure's password" do
-        @c['failure'].password = 'successful'
+        @c['failure'].set_password 'successful'
         assert_equal :authenticated, ShoveAuth::login('failure', 'successful')
+        @c['failure'].set_password 'failure'
+        assert_equal :authenticated, ShoveAuth::login('failure', 'failure')
       end
-      should 'create a pal for failure' do
-        @c.create 'loser'
-        @c['loser'].password = 'winner'
-        assert_equal :authenticated, ShoveAuth::login('loser', 'winner')
+      should 'create and destroy a pal for failure' do
+        tempname = Namegen.screenname + '_TEMP'
+        @c.create tempname
+        @c[tempname].set_password 'winner'
+        assert_equal :authenticated, ShoveAuth::login(tempname, 'winner')
+        @c[tempname].destroy
+        assert !ShoveAuth::login(tempname,'winner')
       end
     end
   end
