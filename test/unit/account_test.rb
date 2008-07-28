1c7fa005ed7201c5ffb3a839cba269f7e36ddd16
Bryce Kerley <bryce@worldmedia.net>
2008-07-28 12:01:12 -0400

    Refactor account tests to use shoulda

--- test/unit/account_test.rb
+++ test/unit/account_test.rb
@@ -1,22 +1,31 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  def setup
-    params = {:username=>'bkerley', :password=>'butt'}
-    @account = Account.new(params)
-    @account.save
-  end
+  should_have_many :nonces
   
-  def test_creation
-    a = Account.find_by_username('bkerley')
-    assert_equal a.id, @account.id
-  end
-  
-  def test_hashing
-    myhash = "jK+2mdV5UlqMcoet5RmsqejZ7Gs="
-    assert_equal Account.digest('bkerley', 'butt'), myhash
-    assert_equal @account.digest, myhash
-    assert @account.check_password('butt')
-    assert @account.check_digest(myhash)
+  context 'a new account' do
+    setup do
+      params = {:username=>'bkerley', :password=>'butt'}
+      @account = Account.new(params)
+      @account.save      
+    end
+    
+    should 'have been created' do
+      a = Account.find_by_username('bkerley')
+      assert_equal a.id, @account.id
+    end
+    
+    should 'hash correctly' do
+        myhash = "jK+2mdV5UlqMcoet5RmsqejZ7Gs="
+        assert_equal Account.digest('bkerley', 'butt'), myhash
+        assert_equal @account.digest, myhash
+        assert @account.check_password('butt')
+        assert @account.check_digest(myhash)
+    end
+    
+    should 'get destroyed' do
+      @account.destroy
+      assert_nil Account.find_by_username 'bkerley'
+    end
   end
 end
