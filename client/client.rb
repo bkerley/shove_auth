8bbaaece6239ff36f8ccc816ff632e482bf882fb
Bryce Kerley <bryce@worldmedia.net>
2008-07-28 16:53:08 -0400

    Pass more tests

--- client/client.rb
+++ client/client.rb
@@ -20,6 +20,8 @@ module ShoveAuth
     return @@site
   end
   
+  class Error < RuntimeError; end
+  
   class Session < ActiveResource::Base
     include ShoveAuth
     self.site = ShoveAuth.site
@@ -46,6 +48,47 @@ module ShoveAuth
     include ShoveAuth
     self.site = ShoveAuth.site
     self.collection_name = 'user'
+    attr_accessor :session
+    
+    def self.[](session)
+      returning User.new do |new_user|
+        new_user.session = session
+      end
+    end
+    
+    def create
+      self.id = username
+      hash = {:session=>{
+        :hmac=>hmac(@session.session_secret, "POST /user/#{self.username} #{@session.sid}"),
+        :sid=>@session.sid
+      }}
+      returning connection.post(element_path, hash.to_xml, self.class.headers) do |response|
+        load_attributes_from_response(response)
+        self.id = self.username
+      end
+    end
+    
+    def find_by_username(username)
+      self.id = username
+      path = element_path(:session=>{
+        :hmac=>hmac(@session.session_secret, "GET /user/#{username} #{@session.sid}"),
+        :sid=>@session.sid
+      })
+      load connection.get(path)
+      self.id = CGI.escape(self.username)
+      return self
+    rescue ActiveResource::ForbiddenAccess
+      return nil
+    end
+    
+    def set_password(new_password)
+      hmac = hmac(@session.session_secret, "PUT /user/#{self.username} #{@session.sid} #{new_password}")
+      puts hmac
+      fields = {:password=>new_password, :session=>{:hmac=>hmac, :sid=>@session.sid}}.to_xml
+      returning connection.put(element_path(prefix_options), fields, self.class.headers) do |response|
+        load_attributes_from_response(response)
+      end
+    end
   end
   
   class Client
@@ -59,6 +102,22 @@ module ShoveAuth
       check = @session.authenticate(username, password)
       @username = username if check
       return check
+    end
+    
+    def [](username)
+      return User[@session].find_by_username username
+    end
+    
+    def create(username)
+      raise ShoveAuth::Error, "user exists" if User[@session].find_by_username username
+      returning User[@session] do |new_user|
+        new_user.username = username
+        new_user.save
+      end
+    end
+    
+    def user
+      return @user ||= self[@username]
     end
   end
   
