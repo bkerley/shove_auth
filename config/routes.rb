bc2abc55b6bbc99c8aaba5070a5f8d6cae800a6c
Bryce Kerley <bryce@worldmedia.net>
2008-07-28 16:52:40 -0400

    Catch a bad post

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :nonces
   map.resources :accounts
   map.resources :admin
+  map.user_creation '/user/:id.:format', :controller=>'user', :action=>'create', :conditions=>{:method=>:post}
   
   map.error '*url', :controller=>'session', :action=>'nil'
 end
