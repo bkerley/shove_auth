194e49bccc9c3da4607461ac408363f51ae93a49
Bryce Kerley <bryce@worldmedia.net>
2008-07-28 10:31:40 -0400

    Failing user update tests

--- test/functional/user_controller_test.rb
+++ test/functional/user_controller_test.rb
@@ -43,5 +43,21 @@ class UserControllerTest < ActionController::TestCase
       should_respond_with :success
       should_respond_with_xml_for :account
     end
+    
+    context 'update user' do
+      setup do
+        request_xml
+        @old_digest = @nonce.account.digest
+        @new_pass = Nonce.rand_bytes
+        put :update, {:id=>@username, :password=>@new_pass, 
+          :session=>{:sid=>@nonce.sid, :hmac=>Nonce.hmac(@nonce.session_secret, "PUT /user/#{@username} #{@nonce.sid} #{@new_pass}")}}
+      end
+      
+      should_respond_with :success
+      should_respond_with_xml_for :account
+      should 'have changed the digest' do
+        assert_not_equal @old_digest, @nonce.account.digest
+      end
+    end
   end
 end
