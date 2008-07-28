2c8943594236d63f4bfa4056e209f270253d70de
Bryce Kerley <bryce@worldmedia.net>
2008-07-28 10:42:53 -0400

    Test more nonce class methods

--- test/unit/nonce_test.rb
+++ test/unit/nonce_test.rb
@@ -41,4 +41,34 @@ class NonceTest < ActiveSupport::TestCase
     end
     
   end
+  
+  context 'rand_bytes' do
+    should 'output forty bytes' do
+      # can't actually test for randomness
+      b = Nonce.rand_bytes
+      assert_equal 40, b.length
+    end
+    
+    should 'be url-safe' do
+      b = Nonce.rand_bytes
+      assert !(b.include? '/')
+      assert !(b.include? '+')
+    end
+  end
+  
+  should 'hmac correctly' do
+    vectors = [
+      # examples from appendix A of FIPS-198a
+      # http://csrc.nist.gov/publications/fips/fips198/fips-198a.pdf
+      {:k=>(0x00..0x3f), :t => 'Sample #1', :c=>'4f4ca3d5d68ba7cc0a1208c9c61e9c5da0403c0a'},
+      {:k=>(0x30..0x43), :t => 'Sample #2', :c=>'0922d3405faa3d194f82a45830737d5cc6c75d24'},
+      {:k=>(0x50..0xb3), :t => 'Sample #3', :c=>'bcf41eab8bb2d802f3d05caf7cb092ecf8d1a3aa'}
+    ]
+    vectors.each do |v|
+      k = v[:k].map{|e|e.chr}.join('')
+      t = v[:t]
+      c = v[:c]
+      assert_equal c, Nonce.hmac(k,t)
+    end
+  end
 end
