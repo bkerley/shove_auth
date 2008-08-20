4634575cbde9b22931c7a52f90cbfbe098464ec6
Bryce Kerley <bryce@worldmedia.net>
2008-08-20 15:27:41 -0400

    membership editing works

--- public/javascripts/membership_editor.js
+++ public/javascripts/membership_editor.js
@@ -6,18 +6,7 @@ function locate_memberships() {
 		var groups = m.select('span');
 		
 		user_memberships[user_id] = m;
-		groups.each(function(g) {
-			var group_id = g.id.gsub(/^membership_\d+_/, '');
-			var delete_button = Builder.node(
-				'a', {
-					className:'delete_group',
-					href:'#',
-					id: id_for_user_and_group('remove', user_id, group_id)
-				},
-				'X');
-			g.appendChild(delete_button);
-			delete_button.observe('click', function(f) {remove_group(user_id, group_id)});
-		});
+		groups.each(group_connector(user_id));
 		
 		var add_button = Builder.node(
 			'a', {
@@ -32,27 +21,86 @@ function locate_memberships() {
 }
 
 function show_add_ui(user_id) {
+	var memberships = $(id_for_user('memberships', user_id));
 	var group_name = Builder.node('input', {type:'text'});
-	var go_button = Builder.node('input', {type:'button', name:'add', value:'+'});
+	var go_button = Builder.node('input', {type:'button', name:'add', value:'Join to group'});
+	var no_button = Builder.node('input', {type:'button', name:'cancel', value:'Don\'t join group'})
 	var autocompleted_names = Builder.node('div');
-	var group_form = Builder.node('form', {className: 'add_form', style:'display: none'}, [group_name, autocompleted_names, go_button]);
-	$(id_for_user('memberships', user_id)).appendChild(group_form);
+	var group_form = Builder.node('form', {className: 'add_form', style:'display: none'},
+		[group_name, autocompleted_names, go_button, no_button]);
+	memberships.insert({top: group_form});
+	
 	new Ajax.Request('/memberships/groups.json', {
 		method: 'get',
 		evalJSON: true,
 		onSuccess: function(transport) {
 			new Autocompleter.Local(group_name, autocompleted_names, transport.responseJSON);
 		}
-	})
+	});
 	Effect.Appear(group_form, {duration: 0.5});
+	
+	no_button.observe('click', function(f) {
+		Effect.Fade(group_form, {duration: 0.5, afterFinish: function(f){group_form.remove();}});
+	});
+	go_button.observe('click', function(f) {
+		var name = group_name.value;
+		ajax_addition(user_id, name, function(group_id) {
+			Effect.Fade(group_form, {duration: 0.5, afterFinish: function(f){group_form.remove();}});
+			var new_span = 
+				Builder.node('span', {id: id_for_user_and_group('membership', user_id, group_id)}, '%'+name);
+			
+			memberships.select('.add_group')[0].insert({before: new_span});
+			connect_group(new_span);
+		});
+	})
 }
 
 function remove_group(user_id, group_id) {
-	alert("Removing user "+user_id+" from group "+group_id);
 	var group_element = $(id_for_user_and_group('membership', user_id, group_id));
-	var effect = Effect.Fade(group_element, {
-		afterFinish: function(f){ group_element.remove(); }
+	ajax_removal(user_id, group_id, function() {
+		Effect.Fade(group_element, {
+			afterFinish: function(f){ group_element.remove(); }
+		});
 	});
+}
+
+function ajax_removal(user_id, group_id, callback) {
+	new Ajax.Request('/memberships/'+group_id+'.json', {
+		parameters: {
+			authenticity_token:($('csrf_token').value)
+		},
+		method: 'delete',
+		onSuccess: callback()
+	});
+}
+
+function ajax_addition(user_id, group_name, callback) {
+	new Ajax.Request('/memberships.json', {
+		method: 'post',
+		parameters: {
+			'membership[account_id]': user_id, 
+			'membership[group]': group_name,
+			authenticity_token:($('csrf_token').value)
+		},
+		onSuccess: function(transport) {callback(transport.responseJSON.id);}
+	});
+}
+
+function group_connector(user_id) {
+	return function(g) {connect_group(g, user_id)};
+}
+
+function connect_group(g, user_id) {
+	var group_id = g.id.gsub(/^membership_\d+_/, '');
+	var delete_button = Builder.node(
+		'a', {
+			className:'delete_group',
+			href:'#',
+			id: id_for_user_and_group('remove', user_id, group_id)
+		},
+		'X');
+	g.appendChild(delete_button);
+	delete_button.observe('click', function(f) {remove_group(user_id, group_id)});
 }
 
 function id_for_user_and_group(prefix, user_id, group_id) {
