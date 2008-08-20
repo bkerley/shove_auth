5633f035c21c3a10629084fa0aa9eab66e0c7d5c
Bryce Kerley <bryce@worldmedia.net>
2008-08-20 11:00:21 -0400

    array of existing groups

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,4 +4,9 @@ class Membership < ActiveRecord::Base
   def inspect
     "%#{group}"
   end
+  
+  # return an array already assigned groups
+  def self.groups
+    connection.select_values("SELECT DISTINCT `group` FROM memberships")
+  end
 end
