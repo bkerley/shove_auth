6276bad52d46982f96961e5e74a3e02f29df78b4
Bryce Kerley <bryce@worldmedia.net>
2008-08-20 09:56:40 -0400

    pass association test

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,6 @@
 class Account < ActiveRecord::Base
   has_many :nonces, :dependent=>:destroy, :foreign_key=>'user_id'
+  has_many :memberships, :dependent=>:destroy
   
   def password
   end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,2 +1,3 @@
 class Membership < ActiveRecord::Base
+  belongs_to :account
 end
