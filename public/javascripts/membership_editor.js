c2395a1e87f3ae1343371a0e1272512c2b33f011
Bryce Kerley <bryce@worldmedia.net>
2008-08-20 11:57:11 -0400

    stub button for adding memberships

--- public/javascripts/membership_editor.js
+++ public/javascripts/membership_editor.js
@@ -1,7 +1,3 @@
-function id_for_user_and_group(prefix, user_id, group_id) {
-	return prefix+user_id+"_"+group_id;
-}
-
 function locate_memberships() {
 	var membership_blocks = $$('.memberships');
 	var user_memberships = $H();
@@ -12,21 +8,41 @@ function locate_memberships() {
 		user_memberships[user_id] = m;
 		groups.each(function(g) {
 			var group_id = g.id.gsub(/^membership_\d+_/, '');
-			var button = Builder.node(
+			var delete_button = Builder.node(
 				'a', {
 					className:'delete_group',
 					href:'#',
-					id: id_for_user_and_group('remove_', user_id, group_id)
+					id: id_for_user_and_group('remove', user_id, group_id)
 				},
 				'X');
-			g.appendChild(button);
-			button.observe('click', function(f) {remove_group(user_id, group_id)});
+			g.appendChild(delete_button);
+			delete_button.observe('click', function(f) {remove_group(user_id, group_id)});
 		});
 		
-		Effect.Pulsate(m);
+		var add_button = Builder.node(
+			'a', {
+				className:'add_group',
+				href:'#',
+				id: id_for_user('add', user_id)
+			},
+			'+');
+		m.appendChild(add_button);
+		add_button.observe('click', function(f) {show_add_ui(user_id)});
 	});
+}
+
+function show_add_ui(user_id) {
+	alert("Add a group to "+user_id);
 }
 
 function remove_group(user_id, group_id) {
 	alert("Removing user "+user_id+" from group "+group_id);
+}
+
+function id_for_user_and_group(prefix, user_id, group_id) {
+	return id_for_user(prefix, user_id)+"_"+group_id;
+}
+
+function id_for_user(prefix, user_id) {
+	return prefix+'_'+user_id;
 }
