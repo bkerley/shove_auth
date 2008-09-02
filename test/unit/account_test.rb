0282fb3540a37e64c2baf9960971c4b0f3345897
Bryce Kerley <bryce@worldmedia.net>
2008-09-02 16:09:18 -0400

    validate username uniqueness

--- test/unit/account_test.rb
+++ test/unit/account_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class AccountTest < ActiveSupport::TestCase
   should_have_many :nonces
   should_have_many :memberships
+  should_require_unique_attributes :username
   
   context 'a new account' do
     setup do
