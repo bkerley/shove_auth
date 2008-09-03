ea17aa82d62ffea05a8c38a86262b300a324bc16
Bryce Kerley <bryce@worldmedia.net>
2008-09-03 15:34:33 -0400

    filled out legacy fields

--- db/migrate/20080903192430_legacy_fields.rb
+++ db/migrate/20080903192430_legacy_fields.rb
@@ -1,7 +1,20 @@
 class LegacyFields < ActiveRecord::Migration
   def self.up
+    #session[:token], session[:user], session[:company_id], session[:rights]
+    adder = proc {|f, t| add_column :accounts, f, t}
+    adder[:token, :string]
+    adder[:company_id, :integer]
+    adder[:first_name, :string]
+    adder[:last_name, :string]
+    adder[:admin, :boolean]
   end
 
   def self.down
+    dropper = proc {|f| drop_column :accounts, f}
+    dropper[:token, :string]
+    dropper[:company_id, :integer]
+    dropper[:first_name, :string]
+    dropper[:last_name, :string]
+    dropper[:admin, :boolean]
   end
 end
