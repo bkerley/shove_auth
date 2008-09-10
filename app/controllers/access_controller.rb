0093dc0ed3508cd59ee827a9408e7f08f22670bf
Bryce Kerley <bryce@worldmedia.net>
2008-09-10 16:16:15 -0400

    pass/fail implemented

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,5 +1,19 @@
 class AccessController < ApplicationController
   def index
+    sid = params[:sid]
+    resource_selector = params[:resource_selector]
+    
+    a = Nonce.find_by_sid(sid).account
+    result = CACKLE.test(a, resource_selector)
+    
+    return(render :text=>'200 OK', :status=>200) if result
+    
+    return fail_403
   end
-
+  
+  private
+  def fail_403
+    render :text=>'403 Forbidden', :status=>403
+    return false
+  end
 end
