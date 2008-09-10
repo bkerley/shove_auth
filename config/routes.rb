e9dbb8b994cd4339f323e87e4d0c03e4479459c7
Bryce Kerley <bryce@worldmedia.net>
2008-09-10 16:16:52 -0400

    route to acl oracle

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :nonces
   map.resources :accounts
   map.resources :admin
+  
+  map.resources :access
   map.user_creation '/user/:id.:format', :controller=>'user', :action=>'create', :conditions=>{:method=>:post}
   
   map.error '*url', :controller=>'session', :action=>'nil'
