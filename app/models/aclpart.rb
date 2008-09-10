691190c6067d96696cb7bf5fe03a3b5a667d8ef1
Bryce Kerley <bryce@worldmedia.net>
2008-09-10 15:30:19 -0400

    aclpart validations

--- app/models/aclpart.rb
+++ app/models/aclpart.rb
@@ -1,2 +1,15 @@
 class Aclpart < ActiveRecord::Base
+  validates_presence_of :title
+  validates_numericality_of :order
+  validates_each :body do |record, attr, value|
+    record.errors.add attr, 'syntax error in body' unless CACKLE.try_parse(value)
+  end
+  
+  def self.concatenated
+    self.find(:all, :order=>"order asc, id asc").map(&:body).join("\n")
+  end
+  
+  def syntax_check
+    CACKLE.try_parse(self.body)
+  end
 end
