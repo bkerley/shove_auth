408a8d55439791743d80c873d78b8ac384bc932d
Bryce Kerley <bryce@worldmedia.net>
2008-09-10 16:17:08 -0400

    test acls

--- vendor/plugins/cackle_shim/lib/cackle_shim.rb
+++ vendor/plugins/cackle_shim/lib/cackle_shim.rb
@@ -9,9 +9,23 @@ class CackleShim
   
   def reload
     data = Aclpart.concatenated
-    parsetree = try_parse(data)
+    parsetree = try_parse(data).selection_array
     throw 'ACL Syntax Error' unless parsetree
     
     @rules = Cackle::RuleList.new(parsetree)
+  end
+  
+  def test(account, resource_selector)
+    reload unless @rules
+    
+    results = account.acl_groups.map do |n|
+      @rules.soft_test(resource_selector, n)
+    end
+    
+    return false if results.include? false
+    return true if results.include? true
+    
+    # everything is a soft fail
+    return false
   end
 end
